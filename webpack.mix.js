bd6c587c2419238de19ef5e0cb8ac555ee54ceb0
羽生優太 <>
2022-02-21 00:29:52 +0900

    TS移行（初期設定完了）

--- webpack.mix.js
+++ webpack.mix.js
@@ -1,16 +1,45 @@
 const mix = require('laravel-mix');
 
-/*
- |--------------------------------------------------------------------------
- | Mix Asset Management
- |--------------------------------------------------------------------------
- |
- | Mix provides a clean, fluent API for defining some Webpack build steps
- | for your Laravel application. By default, we are compiling the Sass
- | file for the application as well as bundling up all the JS files.
- |
- */
+// /*
+//  |--------------------------------------------------------------------------
+//  | Mix Asset Management
+//  |--------------------------------------------------------------------------
+//  |
+//  | Mix provides a clean, fluent API for defining some Webpack build steps
+//  | for your Laravel application. By default, we are compiling the Sass
+//  | file for the application as well as bundling up all the JS files.
+//  |
+//  */
 
-mix.js('resources/js/app.js', 'public/js')
-    .vue()
-    .sass('resources/sass/app.scss', 'public/css');
+// mix.ts('resources/js/app.ts', 'public/js')
+//     .vue()
+//     .sass('resources/sass/app.scss', 'public/css');
+
+
+mix.ts('resources/ts/app.ts', 'public/js').vue()
+  .sass('resources/sass/app.scss', 'public/css');
+// mix.ts("resources/ts/app.ts", "public/js");
+//     .vue()
+//     .sass('resources/sass/app.scss', 'public/css');
+
+mix.webpackConfig({
+  resolve: {
+    extensions: [".js", ".jsx", ".vue", ".ts", ".tsx"],
+    alias: {
+      'vue$': "vue/dist/vue.esm.js"
+    }
+  },
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        loader: "ts-loader",
+        // options: { appendTsSuffixTo: [/\.vue$/] },
+        options: {
+          appendTsSuffixTo: [/\.vue$/] //①optionsを追加
+        },
+        exclude: /node_modules/
+      }
+    ]
+  }
+});
