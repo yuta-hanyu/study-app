780848cba09e15b286bdc843a2ccf13479b1aae9
yuta-hanyu <remakeremake9011@gmail.com>
2022-05-04 23:18:42 +0900

    GAタグ

--- resources/ts/app.ts
+++ resources/ts/app.ts
@@ -4,8 +4,20 @@ import App from './App.vue';
 import Vuetify from 'vuetify';
 import '@mdi/font/css/materialdesignicons.css';
 import store from './store';
+import VueGtag from 'vue-gtag';
+import VueGtm from 'vue-gtm';
+// Vue.use(Vuetify);
 
-Vue.use(Vuetify);
+Vue.use(
+  Vuetify,
+  // VueGtag,
+  // {
+  //   config: { id: "GTM-MJJZBCG" },
+  // },
+  VueGtm, {
+    id: 'GTM-MJJZBCG',
+  }
+);
 
 new Vue({
   vuetify: new Vuetify(),
